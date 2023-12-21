d94ff52b93915f33da6a196e45bb0734cda7561c
Никита <@DESKTOP-Q8K3E9H>
2023-12-21 17:39:11 +0400

    Добавьте файлы проекта.

---
+++ Header.h
@@ -0,0 +1,7 @@
+#pragma once
+int my_strlen(char* str);
+void my_strcpy(char* str1, char* str2);
+void my_strcat(char* str1, char* str2);
+int my_strcmp(char* str1, char* str2);
+char* my_strchr(char* str, char c);
+char* my_strstr(char* str1, char* str2);
